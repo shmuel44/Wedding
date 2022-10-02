d07bf0cbec3b728670b0a958fe5856bf95ae0e92
shmuel44 <s0535577872@gmail.com>
2022-10-02 15:34:33 +0300

    fix date

--- Wedding.js
+++ Wedding.js
@@ -16,13 +16,12 @@ var x = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    document.getElementById("counter").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("counter").innerHTML = "EXPIRED";
     }
 }, 1000);
